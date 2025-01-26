33a2b0c7be3be9b74f4a03c97449bfaadf5efc37
DariaSzw <162465415+DariaSzw@users.noreply.github.com>
2025-01-26 11:20:47 +0100

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,26 +1,20 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-// ==============================================
-//
-//  Version 1.1, 2025-01-16
-//
-// ==============================================
-
 #include <pthread.h>
 
 struct TQueue {
-	pthread_t subscribers[16]; //liczba możliwych subskrybentów
-	int sub;
-    void **mess;
-	int count_mess;
-	int max_size;
-	pthread_t **who_can_read;
-	int *read_counters;
+	pthread_t subscribers[2000]; //liczba możliwych subskrybentów
+	int sub; //liczba subskrybentów
+    	void **mess; //tablica wskaźników - wiadomości
+	int count_mess; //liczba wiadomości
+	int max_size; //maksymalna wielkość kolejki wiadomości
+	pthread_t **who_can_read; //tablica dwuwymiarowa z informacjami kto może odczytać daną wiadomość
+	int *read_counters; //tablica informująca ile osób może odczytać daną wiadomość
 
 	pthread_mutex_t mutex; 
-    pthread_cond_t delete_mess; //gdy wątek chce dodać wiadomość, ale kolejka jest pełna, czeka na sygnał usunięcia wiadomości
-    pthread_cond_t new_mess; //gdy wątek chce pobrać wiadomość, ale kolejka jest pusta, czeka na sygnał dodania nowej wiadomości
+    	pthread_cond_t delete_mess; //gdy wątek chce dodać wiadomość, ale kolejka jest pełna, czeka na sygnał usunięcia wiadomości
+   	 pthread_cond_t new_mess; //gdy wątek chce pobrać wiadomość, ale kolejka jest pusta, czeka na sygnał dodania nowej wiadomości
 };
 typedef struct TQueue TQueue;
 
